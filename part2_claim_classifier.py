e07e268cfaa1760dfe47d9effdd10b47f846b7e3
ludomitch <lmitchener@interrodata.com>
2019-11-24 15:13:57 +0000

    Part 2 done and clean

--- part2_claim_classifier.py
+++ part2_claim_classifier.py
@@ -1,6 +1,5 @@
 import numpy as np
-import pandas as pd
-from sklearn.metrics import confusion_matrix, roc_auc_score, cohen_kappa_score
+from sklearn.metrics import confusion_matrix, roc_auc_score
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split, ParameterGrid
 
@@ -11,8 +10,11 @@ from keras.callbacks import TensorBoard
 
 MODEL_FILE_NAME = "part2_claim_classifer.h5"
 
+
 class ClaimClassifier:
-    def __init__(self, epochs=50, batch_size=64, architecture=[4, 4], dropout=True, model=None):
+    def __init__(
+        self, epochs=50, batch_size=64, architecture=[4, 4], dropout=True, model=None
+    ):
         """
         Feel free to alter this as you wish, adding instance variables as
         necessary. 
@@ -23,19 +25,18 @@ class ClaimClassifier:
         self.dropout = dropout
         self.model = model
 
-    def over_sampler(self, features, labels):
-        unique, counts = np.unique(labels, return_counts=True)
+    @staticmethod
+    def over_sampler(features, labels):
+        _, counts = np.unique(labels, return_counts=True)
         nb_to_pick = counts[0] - counts[1]
-        idx = np.where(labels==1)[0]
+        idx = np.where(labels == 1)[0]
         random_sampled_features = np.random.choice(idx, nb_to_pick)
-
 
         random_sampled_features = [features[i] for i in random_sampled_features]
 
-
         random_sampled_features = np.array(random_sampled_features)
         features = np.vstack((random_sampled_features, features))
-        labels = np.concatenate((np.ones(nb_to_pick,), labels))
+        labels = np.concatenate((np.ones(nb_to_pick), labels))
 
         size = features.shape[0]
         idx = np.arange(0, size)
@@ -91,21 +92,27 @@ class ClaimClassifier:
 
         Returns
         -------
-        ?
+        model: The keras model fully trained on the input data
         """
+
+        # Code necessary to visualise loss and metrics with tensorboard
         id_str = f"{self.architecture}{self.batch_size}{self.epochs}{self.dropout}"
         tbCallback = TensorBoard(log_dir=f"logs/test{id_str}")
 
+        # Preprocess data
         X_clean, y_raw = self._preprocessor(X_raw, y_raw)
 
-        # Config
+        # Get input and output layer dimensions
         input_dim = X_clean.shape[1]
         num_classes = len(np.unique(y_raw)) - 1
 
+        # Create neural network
         model = Sequential()
+        # Input layer
         model.add(Dense(self.architecture[0], input_dim=input_dim, activation="relu"))
         if self.dropout:
             model.add(Dropout(0.2))
+        # Hidden layer
         model.add(
             Dense(
                 self.architecture[1],
@@ -115,15 +122,17 @@ class ClaimClassifier:
         )
         if self.dropout:
             model.add(Dropout(0.2))
+        # Output layer
         model.add(
             Dense(
                 num_classes, kernel_initializer="glorot_uniform", activation="sigmoid"
             )
         )
+        # Compile model
         model.compile(
-            loss="binary_crossentropy", optimizer="adam", metrics=['accuracy']
+            loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
         )
-
+        # Fit model to data
         model.fit(
             X_clean,
             y_raw,
@@ -134,14 +143,32 @@ class ClaimClassifier:
         )
         self.model = model
         self.save_model(model)
-        
+        return model
+
     def predict(self, X_raw):
+        """Classifier probability prediction function.
+
+        Here you will implement the predict function for your classifier.
+
+        Parameters
+        ----------
+        X_raw : numpy.ndarray
+            A numpy array, this is the raw data as downloaded
+
+        Returns
+        -------
+        numpy.ndarray
+            A one dimensional array of the same length as the input with
+            binary values corresponding to the prediction on whether the individual
+            made a claim or not.
+        """
+
         try:
             X_clean = self._preprocessor(X_raw)
             predictions = self.model.predict_classes(X_clean)
 
         except AttributeError:
-            raise (
+            raise Exception(
                 "There is no model saved on this class, please run ClaimClassifier.fit() first."
             )
         return predictions
@@ -161,21 +188,18 @@ class ClaimClassifier:
         numpy.ndarray
             A one dimensional array of the same length as the input with
             values corresponding to the probability of beloning to the
-            POSITIVE class (that had accidents)
-        """
+            POSITIVE class (that had accidents)"""
 
         try:
             X_clean = self._preprocessor(X_raw)
             predictions = self.model.predict(X_clean)
 
         except AttributeError:
-            raise (
+            raise Exception(
                 "There is no model saved on this class, please run ClaimClassifier.fit() first."
             )
 
         return predictions
-
-
 
     def evaluate_architecture(self, X_raw, y_raw):
         """Architecture evaluation utility.
@@ -186,9 +210,12 @@ class ClaimClassifier:
         You can use external libraries such as scikit-learn for this
         if necessary.
         """
+        # Preprocess data
         X_clean = self._preprocessor(X_raw)
+        # Generate predictions
         predictions_binary = self.predict(X_clean)
         predictions_proba = self.predict_proba(X_clean)
+        # Retrieve evaluation metrics
         cm = confusion_matrix(y_raw, predictions_binary)
         roc_auc = roc_auc_score(y_raw, predictions_proba)
 
@@ -196,7 +223,8 @@ class ClaimClassifier:
         print(f"Confusion matrix \n {cm}")
         return roc_auc, cm
 
-    def save_model(self, model):
+    @staticmethod
+    def save_model(model):
         model.save(MODEL_FILE_NAME)
 
 
@@ -208,38 +236,44 @@ def ClaimClassifierHyperParameterSearch():  # ENSURE TO ADD IN WHATEVER INPUTS Y
 
     The function should return your optimised hyper-parameters. 
     """
-
+    # Load data
     data = np.genfromtxt("part2_data.csv", delimiter=",")
-    features = data[1:, :-3]
+    features = data[1:, :-2]
     labels = data[1:, -1]
 
+    # Separate data into training and testing data
     x_train, x_test, y_train, y_test = train_test_split(
         features, labels, test_size=0.1, random_state=12
     )
 
+    # Define hyperparameters dictionary
     hyperparameters = {
-        "epochs": [100, 200, 400],
-        "batch_size": [32, 64, 128],
-        "architecture": [[4, 4], [4, 8], [4, 12]],
+        "epochs": [100, 200],
+        "batch_size": [4, 8, 32, 64, 128],
+        "architecture": [[2, 4], [2, 2], [4, 4], [4, 8]],
         "dropout": [True, False],
     }
-    roc_auc = []
-    accuracy = []
-    scores = []
-    cm = []
+    # Generate all combinations of hyperparameters
     all_params = list(ParameterGrid(hyperparameters))
+
+    # Initialise eval metric lists
+    roc_auc = []
+    cm = []
     for params in all_params:
         print(f"Parameters used:{params}")
+        # Generate class instance with new parameters
         cc = ClaimClassifier(**params)
         cc.fit(x_train, y_train)
-        res = cc.evaluate_architecture(x_test, y_test)
-        roc_auc.append(res[0])
-        scores.append(res[1])
-        accuracy.append(res[1][0])
-        cm.append(res[2])
-    return roc_auc, all_params, scores, accuracy, cm
+        # Evaluate teh architecture
+        roc_i, cm_i = cc.evaluate_architecture(x_test, y_test)
+        roc_auc.append(roc_i)
+        cm.append(cm_i)
+    return all_params, roc_auc, cm
+
 
 def load_model():
+    """Loads the keras model stored."""
+
     model = keras.models.load_model(MODEL_FILE_NAME)
     cc = ClaimClassifier(model=model)
     return cc
